cf1e99861ffe121b5e1ba82a562afcf171f7dc31
Bodo Stöcker <bodo.stoecker@cognizant.com>
2021-07-13 18:57:52 +0200

    Setting message back to simple format.

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hallo, hier ist nochmal Bodo! Bodo Stöcker, genau genommen.'));
+app.get('/', (req, res) => res.send('Hello Bodo!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
