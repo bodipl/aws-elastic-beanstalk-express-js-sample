a7c82e640c432cbd49c26fe5e385440cbb4fab1d
Bodo Stöcker <bodo.stoecker@cognizant.com>
2021-07-13 18:44:29 +0200

    Modified message again for full pipeline test.wq

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hallo, hier ist Bodo!'));
+app.get('/', (req, res) => res.send('Hallo, hier ist nochmal Bodo! Bodo Stöcker, genau genommen.'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
